954a34d8e5109c1b9ee2ba2672b36957e1b64915
Kevin Mathias <komathias1@gmail.com>
2020-03-27 23:47:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@
 def reverse_each_word(sentence)
   array1_rev = []
   array1 = sentence.split
-  array1.each{|word| array1_rev << word.reverse}
+  array1.collect{|word| array1_rev << word.reverse}
   array1_rev.join(" ")
 end
